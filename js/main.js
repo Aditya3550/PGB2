4fc63127d182b182c6bdeb3e094c742776360149
Aditya Bhushan <aditya3550@gmail.com>
2014-09-30 09:44:26 +0530

    Back Button changes
    Back Button changes

--- js/main.js
+++ js/main.js
@@ -8,17 +8,17 @@ var app = {
         }
     },
 
-    registerEvents: function() {
+    registerEvents: function () {
         $(window).on('hashchange', $.proxy(this.route, this));
-        $('body').on('mousedown', 'a', function(event) {
+        $('body').on('mousedown', 'a', function (event) {
             $(event.target).addClass('tappable-active');
         });
-        $('body').on('mouseup', 'a', function(event) {
+        $('body').on('mouseup', 'a', function (event) {
             $(event.target).removeClass('tappable-active');
         });
     },
 
-    route: function() {
+    route: function () {
         var self = this;
         var hash = window.location.hash;
         if (!hash) {
@@ -32,13 +32,13 @@ var app = {
         }
         var match = hash.match(this.detailsURL);
         if (match) {
-            this.store.findById(Number(match[1]), function(employee) {
+            this.store.findById(Number(match[1]), function (employee) {
                 self.slidePage(new EmployeeView(employee).render());
             });
         }
     },
 
-    slidePage: function(page) {
+    slidePage: function (page) {
 
         var currentPageDest,
             self = this;
@@ -46,13 +46,14 @@ var app = {
         // If there is no current page (app just started) -> No transition: Position new page in the view port
         if (!this.currentPage) {
             $(page.el).attr('class', 'page stage-center');
+            //$(page.el).attr('class', 'page stage-center').attr('id', 'homePage'); // add ID !
             $('body').append(page.el);
             this.currentPage = page;
             return;
         }
 
         // Cleaning up: remove old pages that were moved out of the viewport
-        $('.stage-right, .stage-left').not('.homePage').remove();
+        $('.stage-right, .stage-left').not('#homePage').remove();
 
         if (page === app.homePage) {
             // Always apply a Back transition (slide from left) when we go back to the search page
@@ -67,7 +68,7 @@ var app = {
         $('body').append(page.el);
 
         // Wait until the new page has been added to the DOM...
-        setTimeout(function() {
+        setTimeout(function () {
             // Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
             $(self.currentPage.el).attr('class', 'page transition ' + currentPageDest);
             // Slide in the new page
@@ -77,11 +78,11 @@ var app = {
 
     },
 
-    initialize: function() {
+    initialize: function () {
         var self = this;
         this.detailsURL = /^#employees\/(\d{1,})/;
         this.registerEvents();
-        this.store = new MemoryStore(function() {
+        this.store = new MemoryStore(function () {
             self.route();
         });
     }
